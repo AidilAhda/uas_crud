b523a7a3dc634663153417f71624d16ba68d8d73
AidilAhda <76725012+AidilAhda@users.noreply.github.com>
2022-06-21 16:10:15 +0700

    succes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,48 @@
 <template>
   <div class="home">
+    <div class="container">
     <h1 class="page-header text-center">DATA KARYAWAN</h1>
-    
+    <div id="vuejscrudmembers">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Member List
+                    <button class="btn btn-primary pull-right" @click="showAddModal = true"><span class="glyphicon glyphicon-plus"></span> Member</button>
+                    </h2>
+                </div>
+            </div>
+ 
+            <div class="alert alert-danger text-center" v-if="errorMessage">
+                <button type="button" class="close" @click="clearMessage();"><span aria-hidden="true">×</span></button>
+                <span class="glyphicon glyphicon-alert"></span> {{ errorMessage }}
+            </div>
+             
+            <div class="alert alert-success text-center" v-if="successMessage">
+                <button type="button" class="close" @click="clearMessage();"><span aria-hidden="true">×</span></button>
+                <span class="glyphicon glyphicon-ok"></span> {{ successMessage }}
+            </div>
+ 
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr v-for="member in members" :key="member.employee_id">
+                        <td>{{ member.firstname }}</td>
+                        <td>{{ member.lastname }}</td>
+                        <td>
+                            <button class="btn btn-success" @click="showEditModal = true; selectMember(member);"><span class="glyphicon glyphicon-edit"></span> Edit</button> 
+                            <button class="btn btn-danger" @click="showDeleteModal = true; selectMember(member);"><span class="glyphicon glyphicon-trash"></span> Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
   </div>
 </template>
 
